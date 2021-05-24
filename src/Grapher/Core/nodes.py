ef8445fe9ad32a3550ecf584a8d36d9838258c38
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-24 08:08:27 +0800

    Add translator and codifier for each type of nodes

--- src/Grapher/Core/nodes.py
+++ src/Grapher/Core/nodes.py
@@ -19,11 +19,29 @@ class Program:
     def __init__(self, stmts):
         self.stmts = stmts
 
+    @staticmethod
+    def init_code():
+        return "import sympy as _sp;_p=print;_i=input;x=_sp.Symbol(\"x\");"
+
+    def translate(self):
+        code = Program.init_code()
+        for stmt in self.stmts:
+            if isinstance(stmt, (If, While)):
+                code += "\n" + stmt.translate()
+                continue
+            code += stmt.codify()
+        return code
+
 
 class Assignment:
     def __init__(self, name, expr):
         self.name = name
         self.expr = expr
+
+    def codify(self):
+        while type(self.expr) != str:
+            self.expr = self.expr.codify()
+        return f"{get_symbol(self.name)}={self.expr};"
 
 
 class Evaluation:
@@ -35,6 +53,11 @@ class Evaluation:
 class Print:
     def __init__(self, expr):
         self.expr = expr
+
+    def codify(self):
+        while type(self.expr) != str:
+            self.expr = self.expr.codify()
+        return f"_p({self.expr});"
 
 
 class Plot:
@@ -54,6 +77,9 @@ class Input:
         self.name = name
         self.prompt = prompt
 
+    def codify(self):
+        return f"{get_symbol(self.name)}=_i({self.prompt});"
+
 
 class If:
     def __init__(self, condition, true_block, false_block):
@@ -61,11 +87,37 @@ class If:
         self.true_block = true_block
         self.false_block = false_block
 
+    def translate(self):
+        code = f"if {self.condition.codify()}:\n\t"
+        for stmt in self.true_block:
+            if isinstance(stmt, (If, While)):
+                code += stmt.translate()
+                continue
+            code += stmt.codify()
+        if self.false_block is None:
+            return code
+        code += "\nelse:\n\t"
+        for stmt in self.false_block:
+            if isinstance(stmt, (If, While)):
+                code += stmt.translate()
+                continue
+            code += stmt.codify()
+        return code
+
 
 class While:
     def __init__(self, condition, code_block):
         self.condition = condition
         self.code_block = code_block
+
+    def translate(self):
+        code = f"if {self.condition.codify()}:\n\t"
+        for stmt in self.code_block:
+            if isinstance(stmt, (If, While)):
+                code += stmt.translate()
+                continue
+            code += stmt.codify()
+        return code
 
 
 class BinaryOps:
@@ -74,12 +126,35 @@ class BinaryOps:
         self.op = op
         self.right = right
 
+    def codify(self):
+        while type(self.left) != str:
+            self.left = self.left.codify()
+        while type(self.right) != str:
+            self.right = self.right.codify()
+        if self.left in global_symbol_match:
+            self.left = get_symbol(self.left)
+        if self.right in global_symbol_match:
+            self.right = get_symbol(self.right)
+        self.op = "**" if self.op == "^" else self.op
+        return str(sympify(f"({self.left}){self.op}({self.right})"))
+
 
 class Comparison:
     def __init__(self, left, op, right):
         self.left = left
         self.op = op
         self.right = right
+
+    def codify(self):
+        while type(self.left) != str:
+            self.left = self.left.codify()
+        while type(self.right) != str:
+            self.right = self.right.codify()
+        if self.left in global_symbol_match:
+            self.left = get_symbol(self.left)
+        if self.right in global_symbol_match:
+            self.right = get_symbol(self.right)
+        return f"{self.left}{self.op}{self.right}"
 
 
 class InvalidToken(BaseException):
@@ -98,3 +173,7 @@ class UndefinedName(BaseException):
 
     def __str__(self):
         return f"Undefined name '{self.token.value}' at line {self.token.source_pos.lineno}"
+
+
+def generate_python_code(ast):
+    return ast.translate()
