211d6052b5e290f939e75c3c1f9baf018d8ae3d1
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-24 18:05:06 +0800

    Expose core functions

--- src/Grapher/Core/__init__.py
+++ src/Grapher/Core/__init__.py
@@ -1,3 +1,14 @@
 from .lexer import Lexer, get_source_signature
 from .parser import Parser, generate_ast
-from .nodes import generate_python_code, populate_matching_table
+from .nodes import generate_python_code
+from .serialiser import serialise_ast, deserialise_ast
+
+__all__ = [
+    "Lexer",
+    "get_source_signature",
+    "Parser",
+    "generate_ast",
+    "generate_python_code",
+    "serialise_ast",
+    "deserialise_ast",
+]
