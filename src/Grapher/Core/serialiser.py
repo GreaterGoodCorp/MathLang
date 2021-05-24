d42ed9ba2b7aceab80fe13c51612ecdaac33b701
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-24 18:03:35 +0800

    Add AST serialiser and deserialiser

---
+++ src/Grapher/Core/serialiser.py
@@ -0,0 +1,39 @@
+from json import JSONEncoder, loads, dumps
+
+from Grapher.Core.nodes import *
+
+
+class ASTSerialiser(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, AST):
+            return o.serialise()
+        else:
+            raise TypeError(f"Object of type '{type(o)}' is not serialisable")
+
+
+def json_decode_hook(o):
+    obj_list = [
+        "Assignment",
+        "BinaryOps",
+        "Comparison",
+        "Evaluation",
+        "If",
+        "Input",
+        "Plot",
+        "Print",
+        "Program",
+        "Solve",
+        "While",
+    ]
+    t = o.get("type", None)
+    if t is not None and t in obj_list:
+        return eval(f"{o['type']}(**(o['params']))")
+    return o
+
+
+def serialise_ast(ast: AST) -> str:
+    return dumps(ast, cls=ASTSerialiser)
+
+
+def deserialise_ast(d: str) -> AST:
+    return loads(d, object_hook=json_decode_hook)
