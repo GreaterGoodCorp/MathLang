883bd52ea636c028775627f97e4bdb38c82d3cf7
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-24 18:24:06 +0800

    Rename tests

--- src/Grapher/Tests/test_lexer.py
+++ src/Grapher/Tests/test_lexer.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import pytest
+from pytest import fixture
 
 from Grapher.Core import get_source_signature
 
@@ -9,33 +9,33 @@ test_data_path = Path(__file__).parent.absolute() / "test_data"
 
 class TestLexer:
     @staticmethod
-    @pytest.fixture()
+    @fixture()
     def demo():
         with open(test_data_path / "demo_source.gp") as fp:
             return fp.read()
 
     @staticmethod
-    @pytest.fixture()
+    @fixture()
     def demo_sgn():
         with open(test_data_path / "demo_source.signature") as fp:
             return fp.read()
 
     @staticmethod
-    @pytest.fixture()
+    @fixture()
     def conditional():
         with open(test_data_path / "conditional_source.gp") as fp:
             return fp.read()
 
     @staticmethod
-    @pytest.fixture()
+    @fixture()
     def conditional_sgn():
         with open(test_data_path / "conditional_source.signature") as fp:
             return fp.read()
 
     @staticmethod
-    def test_demo_source(demo, demo_sgn):
+    def test_demo_parsing(demo, demo_sgn):
         assert get_source_signature(demo) == demo_sgn.strip("\n")
 
     @staticmethod
-    def test_conditional_source(conditional, conditional_sgn):
+    def test_conditional_parsing(conditional, conditional_sgn):
         assert get_source_signature(conditional) == conditional_sgn.strip("\n")
