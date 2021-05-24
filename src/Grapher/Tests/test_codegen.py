3a08678535c5ea25490a46b87ed128f56fa6774a
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-24 22:34:49 +0800

    Write code generation tests for conditional source

---
+++ src/Grapher/Tests/test_codegen.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from json import loads, dumps
+
+from pytest import fixture
+
+from Grapher.Core import generate_python_code, deserialise_ast, generate_ast
+
+test_data_path = Path(__file__).parent.absolute() / "test_data"
+
+
+class TestCodegen:
+    @staticmethod
+    @fixture()
+    def quick_src():
+        return "PRINT \"Hello World\";"
+
+    @staticmethod
+    @fixture()
+    def demo_ast():
+        with open(test_data_path / "simple_demo.ast") as fp:
+            return dumps(loads(fp.read()))
+
+    @staticmethod
+    @fixture()
+    def conditional_ast():
+        with open(test_data_path / "simple_conditional.ast") as fp:
+            return dumps(loads(fp.read()))
+
+    @staticmethod
+    @fixture()
+    def redirect_stdout():
+        return "import sys;import io;sys.stdout=io.StringIO();"
+
+    @staticmethod
+    def test_codegen(redirect_stdout, quick_src):
+        code = redirect_stdout + generate_python_code(generate_ast(quick_src))
+        assert exec(code) is None
+
+    @staticmethod
+    def test_conditional_codegen(redirect_stdout, conditional_ast):
+        code = redirect_stdout + generate_python_code(deserialise_ast(conditional_ast))
+        assert exec(code) is None
