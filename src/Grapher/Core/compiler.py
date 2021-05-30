b1e0d13b9d5175f0ed7f839cff67908e33e0a17d
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-30 15:21:45 +0800

    Add class Compiler

---
+++ src/Grapher/Core/compiler.py
@@ -0,0 +1,83 @@
+import os
+import warnings
+from typing import Union
+import marshal
+import hmac
+import base64
+
+from Grapher.Core.nodes import AST
+
+
+class Compiler:
+    @staticmethod
+    def compile(source: Union[AST, str]) -> bytes:
+        if isinstance(source, str):
+            bytecode = Compiler.__compile(source)
+        else:
+            bytecode = Compiler.__compile(source.codify())
+        signature = Compiler.__sign(bytecode, True)
+        return bytecode + signature
+
+    @staticmethod
+    def decompile(source: bytes, unsafe: bool = False) -> bytes:
+        bytecode, signature = source[:-64], source[-64:]
+        try:
+            ctrl_signature = Compiler.__sign(bytecode, False)
+            assert hmac.compare_digest(signature, ctrl_signature)
+        except UnverifiedSignatureWarning:
+            if not unsafe:
+                raise UnsafeDecompilationError("No signing key found. "
+                                               "The program must be decompiled in 'unsafe' mode.")
+            else:
+                warnings.warn("No signing key found. Signature verification will be skipped.")
+        except AssertionError:
+            if not unsafe:
+                raise UnsafeDecompilationError("Signature verification failed. "
+                                               "The program must be decompiled in 'unsafe' mode")
+            else:
+                warnings.warn("Signature verification failed. Proceed with execution carefully.")
+        return Compiler.__decompile(bytecode)
+
+    @staticmethod
+    def get_signing_key():
+        return Compiler.__get_random_signature_key()
+
+    @staticmethod
+    def __compile(source):
+        return marshal.dumps(compile(source, "<Grapher>", "exec", optimize=2))
+
+    @staticmethod
+    def __decompile(bytecode):
+        return marshal.loads(bytecode)
+
+    @staticmethod
+    def __sign(source, is_compile):
+        return hmac.new(Compiler.__get_signature_key(is_compile), source, "sha256").digest()
+
+    @staticmethod
+    def __get_signature_key(is_compile):
+        s = base64.b64decode(os.getenv("GRAPHER_SIGNING_KEY"))
+        if s is None:
+            if is_compile:
+                warnings.warn(
+                    "No singing key found, a random key is used. "
+                    "The program must be executed in 'unsafe' mode.",
+                    UnverifiedSignatureWarning)
+                return Compiler.__get_random_signature_key()
+            else:
+                raise UnverifiedSignatureWarning
+        return s
+
+    @staticmethod
+    def __get_random_signature_key():
+        return os.urandom(32)
+
+
+class UnverifiedSignatureWarning(Warning):
+    def __init__(self, *args):
+        super(UnverifiedSignatureWarning, self).__init__(*args)
+
+
+class UnsafeDecompilationError(BaseException):
+    def __init__(self, *args):
+        super(UnsafeDecompilationError, self).__init__(*args)
