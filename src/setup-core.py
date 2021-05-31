3ee5fa517521adb02953be1565afb03595be6a4d
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-31 12:01:11 +0800

    Initial commit

---
+++ src/setup-core.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Grapher-Core',
+    version='',
+    packages=['Grapher', 'Grapher.Core'],
+    package_dir={'': 'src'},
+    url='https://github.com/GreaterGoodCorp/Grapher',
+    license='MIT License',
+    author='m1k3',
+    author_email='binhnt.mdev@gmail.com',
+    description='Grapher is a specially designed programming language for maths.'
+)
